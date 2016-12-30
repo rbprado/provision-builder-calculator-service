affcdf53fd6d7cf6fe39853957de60903435ef65
Prado <rb.prado@gmail.com>
2016-12-30 17:22:19 -0200

    Create script to verify if the service is up

---
+++ isServiceUp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true; do
+    sleep 5
+    wget http://localhost:8200/calculator/multiply?a=8\&b=6
+    
+    if [ $? -eq 0 ]; then
+        echo 'Service UP'
+        break
+    fi
+done
